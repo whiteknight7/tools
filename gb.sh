5b1381c8eb294f5bd4d10f6d43a903f07af090f8
whiteknight7 <61610692+whiteknight7@users.noreply.github.com>
2021-04-16 18:57:07 +0800

    Delete gb.sh

--- gb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p 'URL: ' url
-
-gobuster dir -u $url -t 1 -w /root/wordlist/common.txt -x .php,.txt,.php.bak,.docx -b 404
-gobuster dir -u $url -t 1 -w /root/wordlist/directory-list-2.3-medium.txt -x .php,.txt,.php.bak,.docx -b 403,404
-gobuster vhost -u $url -t 1 -w /root/wordlist/subdomains-top1million-5000.txt
-gobuster vhost -u $url -t 1 -w /root/wordlist/directory-list-2.3-medium.txt | grep "Status: 200"

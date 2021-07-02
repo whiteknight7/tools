70e04878114b0a2c6ce21fb4bcaba364af0069cc
whiteknight7 <61610692+whiteknight7@users.noreply.github.com>
2021-07-03 00:09:22 +0800

    Create fileSearch.py

---
+++ fileSearch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import subprocess
+import os, sys
+
+testDirs = ["/usr/share", "/var"]
+
+for testDir in testDirs:
+  print(f"Start searching in {testDir} ...")
+  for dirs, subdirs, files in os.walk(testDir):
+    for file in files:
+      filePath = os.path.join(dirs,file)
+      out = subprocess.getoutput(f"file {filePath}")
+      if 'PDF' in out:
+        print(filePath)
+        sys.exit()
+ print("Done.")

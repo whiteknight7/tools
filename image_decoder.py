0e8f9c3e70a301d81e80d0457486f923771b11a3
whiteknight7 <61610692+whiteknight7@users.noreply.github.com>
2021-07-12 14:36:01 +0800

    Update image_decoder.py

--- image_decoder.py
+++ image_decoder.py
@@ -1,9 +1,10 @@
-from pyzbar.pyzbar import decode
+#!/usr/bin/python3
+import pyzbar.pyzbar as pyzbar
 from PIL import Image
-from os import listdir
-from os.path import isfile, join
-mypath="/var/tmp/"
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-for files in onlyfiles:
-        out = decode(Image.open(mypath+files))
-        print(out[0].data)
+for number in range(0,68):
+    fileName = '/root/1/192.168.0.188/justanothergallery/qr/image-{}.png'.format(number)
+    img = Image.open(fileName)
+    barcodes = pyzbar.decode(img)
+    for barcode in barcodes:
+        barcodeData = barcode.data.decode('utf-8')
+        print(barcodeData)
